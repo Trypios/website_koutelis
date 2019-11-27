4dc517aeb83e7f4082c41f7c34c680e77bbc541d
Koutelis <stefanos.koutelis@yahoo.com>
2019-11-27 19:09:38 +0200

    Added aboutSection, pricingSection, social icons, copyrightSection. Fixed header slider, navigator, contactSection.
    Updated README

--- README.md
+++ README.md
@@ -1,14 +1,22 @@
-# koutelis.com
+#koutelis.com
+============
 
-personal website in development
+## personal website in development
 HTML, CSS, JS
 
-<<<<<<< HEAD
 * Bootstrap
 * Bootstrap Navbar
 
-=======
->>>>>>> 3f763b073bbe7e15cf17952eb0c2628b268eb6ec
-using the following JS:
-* superslides https://github.com/nicinabox/superslides
-* typed https://github.com/mattboldt/typed.js/
+## JS used:
+* jquery
+* superslides | https://github.com/nicinabox/superslides
+* typed.js | https://github.com/mattboldt/typed.js/
+* isotope | https://isotope.metafizzy.co/
+* fancybox 3 | https://fancyapps.com/fancybox/3/
+* owl carousel 2 | https://owlcarousel2.github.io/OwlCarousel2/
+* easy-pie-chart | https://github.com/rendro/easy-pie-chart
+
+## useful links:
+* free stock photos | https://www.pexels.com/
+* free icons | https://icons8.com/
+* color palettes | https://flatuicolors.com/
--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
-$(document).ready(function() {
+$(window).on("load", function() {
 
+    $(".loader .inner").fadeOut(600, function() {
+        $(".loader").fadeOut(850);
+        
+    });
+
+    $(".items").isotope({
+
+        filter: '*',
+        animationOptions: {
+            duration:1500,
+            easing: 'linear',
+            queue: false
+        }
+
+    });
+    
+});
+
+$(document).ready(function() {
+    
     $('#slides').superslides({
         animation: 'fade',
         play: 3000,
@@ -43,5 +63,73 @@ $(document).ready(function() {
             body.removeClass("fixedNav");
         }
     }
+
+    $("[data-fancybox]").fancybox();
+
+    $("#filters a").click(function() {
+
+        $("#filters .current").removeClass("current");
+        $(this).addClass("current");
+
+        var selector = $(this).attr("data-filter");
+
+        $(".items").isotope({
+
+            filter: selector,
+            animationOptions: {
+                duration:1500,
+                easing: 'linear',
+                queue: false
+            }
+    
+        });
+
+        return false;
+
+    });
+
+    $('.owl-carousel').owlCarousel({
+        loop: false,
+        items: 4,
+        responsive:{
+            0:{
+                items:1
+            },
+            480:{
+                items:2
+            },
+            768:{
+                items:3
+            },
+            938:{
+                items:4
+            }
+        }
+    });
+
+    var priceTopOffset = $(".pricingSection").offset().top;
+    
+    $(window).scroll(function() {
+
+        if(window.pageYOffset > priceTopOffset - $(window).height() + 200) {
+
+            $('.chart').easyPieChart({
+
+                easing: 'easeInOut',
+                barColor: '#ca4a00',
+                trackColor: false,
+                scaleColor: false,
+                lineWidth: 4,
+                size: 152,
+                onStep: function(from, to, percent) {
+
+                    $(this.el).find('.percent').text(Math.round(percent));
+
+                }
+            });
+
+        }
+
+    });
 
 });
